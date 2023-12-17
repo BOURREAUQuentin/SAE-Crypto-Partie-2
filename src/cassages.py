9ab8fc02d3947f4d6b57e86881da35426f20610c
loannchhum <loann.chhum-moxel@et-univ-orleans.fr>
2023-12-17 11:43:32 +0100

    feat: ajout affichage du nombre de tentatives cassage brutal et astucieux

--- src/cassages.py
+++ src/cassages.py
@@ -15,11 +15,14 @@ def cassage_brutal(message_clair, message_chiffre):
         (tuple): le tuple contenant la 1ère clé et la 2ème clé possiblement utilisé pour le
                  chiffrement du message
     """
+    nb_tentatives = 0
     for premiere_cle in range(0, CONST.NOMBRE_CLES_POSSIBLES): # 2**10 possibilités clé de 10 bits
         for seconde_cle in range(0, CONST.NOMBRE_CLES_POSSIBLES): # même chose que premiere_cle
+            nb_tentatives += 1
             decrypted_text_result = double_dechiffrement_sdes(premiere_cle,
                                                              seconde_cle, message_chiffre)
             if decrypted_text_result == message_clair:
+                print("Nombre de tentatives pour trouver le couple de clés :", nb_tentatives)
                 return seconde_cle, premiere_cle # les clés correctes ont été trouvées
     return None, None  # si aucune clé correcte n'est trouvée
 
@@ -35,15 +38,19 @@ def cassage_astucieux(message_clair, message_chiffre):
         (tuple): le tuple contenant la 1ère clé et la 2ème clé possiblement utilisé pour le
                  chiffrement du message
     """
+    nb_tentatives = 0
     # précalculer les résultats intermédiaires pour le chiffrement avec la première clé
     dict_resultats_intermediaires = {}
     for premiere_cle in range(0, CONST.NOMBRE_CLES_POSSIBLES):
+        nb_tentatives += 1
         texte_chiffre_intermediaire = chiffrement_sdes(premiere_cle, message_clair)
         dict_resultats_intermediaires[texte_chiffre_intermediaire] = premiere_cle
     # tester les déchiffrements avec la deuxième clé et chercher une correspondance
     for seconde_cle in range(0, CONST.NOMBRE_CLES_POSSIBLES):
+        nb_tentatives += 1
         texte_dechiffre_intermediaire = dechiffrement_sdes(seconde_cle, message_chiffre)
         if texte_dechiffre_intermediaire in dict_resultats_intermediaires:
+            print("Nombre de tentatives pour trouver le couple de clés :", nb_tentatives)
             premiere_cle = dict_resultats_intermediaires[texte_dechiffre_intermediaire]
             return seconde_cle, premiere_cle
     return None, None # si aucune clé correcte n'est trouvée
